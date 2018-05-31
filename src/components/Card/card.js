d92201be7a2384ff0f069615b3389649be4ec7dd
David Daugherty <david.m.daugherty@gmail.com>
2018-05-31 12:11:11 -0600

    Add rest of card data and connected styling

--- src/components/Card/card.js
+++ src/components/Card/card.js
@@ -1,9 +1,16 @@
 import React from 'react';
 
-const Card = ({house}) => {
+const Card = ({ house }) => {
   return (
-    <div>
-      <h2>{house.name}</h2>
+    <div className="Card">
+      <h1>{house.name}</h1>
+      <h3>Founded: {house.founded}</h3>
+      <h3>Seats: {house.seat}</h3>
+      <h3>Titles: {house.titles}</h3>
+      <h3>Coat of Arms: {house.coatOfArms}</h3>
+      <h3>Ancestral Weapons: {house.ancestralWeapons}</h3>
+      <h3>Words: {house.words}</h3>
+
     </div>
   );
 };
--- src/components/CardContainer/CardContainer.js
+++ src/components/CardContainer/CardContainer.js
@@ -10,7 +10,7 @@ const CardContainer = (props) => {
   })
   
   return (
-    <div>
+    <div className="Container">
       {cards}
     </div>
   );
